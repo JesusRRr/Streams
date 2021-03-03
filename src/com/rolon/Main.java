47e8e739db992c81ce42282205aa297ea82bbb6e
JesusRRr <rolonjesus1996@gmail.com>
2021-03-02 22:19:33 -0600

    chore: adding filter by age functionality

--- src/com/rolon/Main.java
+++ src/com/rolon/Main.java
@@ -6,12 +6,19 @@ import com.rolon.model.Person;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Person> people = getPeople();
         people.forEach(System.out::println);
+
+        //Filter By Age
+        people.stream()
+                .filter(person -> person.getAge()<40)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
     }
 
     public static List<Person> getPeople(){
