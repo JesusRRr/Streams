bccd66d5a8d299ed7dcaac8794abc4ef8cbb1e47
JesusRRr <rolonjesus1996@gmail.com>
2021-03-02 22:10:03 -0600

    chore: adding getters, setters, constructor in Person class and getPeople method in Main class

--- src/com/rolon/Main.java
+++ src/com/rolon/Main.java
@@ -1,8 +1,24 @@
 package com.rolon;
 
+import com.rolon.model.Gender;
+import com.rolon.model.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Person> people = getPeople();
+        
+    }
+
+    public static List<Person> getPeople(){
+      return Arrays.asList(
+              new Person("Chick Corea", 79, Gender.MALE),
+              new Person("Herbie Hancock",80, Gender.MALE),
+              new Person("Esperanza Spalding", 36, Gender.FEMALE)
+      );
     }
 }
